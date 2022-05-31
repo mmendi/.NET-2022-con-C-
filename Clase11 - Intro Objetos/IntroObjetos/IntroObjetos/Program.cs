bbfd74b6b602220528f3cab09ac77280fa7bb589
mmendi <matiasmendilaharzu@gmail.com>
2022-05-31 20:36:27 -0300

    Clase 11 Intro a Objetos

--- Clase11 - Intro Objetos/IntroObjetos/IntroObjetos/Program.cs
+++ Clase11 - Intro Objetos/IntroObjetos/IntroObjetos/Program.cs
@@ -4,7 +4,43 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Instancia 1
+            Perro firulais = new Perro();
+            firulais.Color = "Negro";
+            firulais.Edad = 4;
+            firulais.Tamanio = "Grande";
+            firulais.Raza = "Maztín Napolitano";
+
+            // Instancia 2
+            Perro fatiga = new Perro();
+            fatiga.Color = "Blanco";
+            fatiga.Edad = 2;
+            fatiga.Tamanio = "Medio";
+            fatiga.Raza = "Maltes";
+
+            // Instancia 3
+            Perro cartucho = new Perro();
+            cartucho.Color = "Marrón";
+            cartucho.Edad = 3;
+            cartucho.Tamanio = "Medio";
+            cartucho.Raza = "Chow Chow";
+
+            // Lista de perros
+            var perritos = new List<Perro>();
+            perritos.Add(firulais);
+            perritos.Add(fatiga);
+            perritos.Add(cartucho);
+
+            Console.WriteLine($"Hay {perritos.Count} perritos en la lista");
+
+            foreach (var item in perritos)
+            {
+                Console.Write(item.Raza + " -> ");
+                item.Ladrar();
+            }
+
+
+
         }
     }
 }
